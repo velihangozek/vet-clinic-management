3bb68b14af31e648bc9bb8988ee5a3869a4d4367
Velihan Gözek <velihangozek@hotmail.com>
2025-06-19 03:17:56 +0300

    spring boot vet-clinic-management project generate via spring initializr

---
+++ src/main/java/org/velihangozek/vet_clinic_management/VetClinicManagementApplication.java
@@ -0,0 +1,13 @@
+package org.velihangozek.vet_clinic_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/velihangozek/vet_clinic_management/VetClinicManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.velihangozek.vet_clinic_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
